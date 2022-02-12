65687d9f80f4f860ffae60d0f16b26f8a87c34ba
adrian88szymanski <adrian88szymanski@gmail.com>
2022-02-12 06:55:13 +0000

    Added class index, detail, results in polls/views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 
 class IndexView(generic.ListView):
